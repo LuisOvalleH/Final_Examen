9b3da1dbb05f2cb48f1aedc964348eb5eea68f6a
Luis Ovalle <luismavalle82@gmail.com>
2022-05-03 09:28:11 -0600

    main.py actualizado
    Se agrego un pequeño programa al archivo main.py

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ print("Estudiante: Luis Ovalle")
 print("Carne de estudiante: 1515922")
 print("Edad: 15 años")
 print("Carrera: Ingenieria en sistemas")
+
+print("Porfavor ingrese el nombre de su animal favorito")
+animal=input()
+if animal=="ornitorrinco":
+    print("A mi tambien me gustan los ornitorrincos")
+else:
+    print("Es un animal interestante, pero me gustan mas los ornitorrincos")
